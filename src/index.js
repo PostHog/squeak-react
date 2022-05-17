e9ee5be8c50f04f93f9bba464559455094711e89
Eli Kinsey <eli@ekinsey.dev>
2022-05-17 15:54:41 -0700

    export QuestionForm (#43)

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
+import { Form } from './components/main/Form'
 import { Question } from './components/main/Question'
 import { Squeak } from './components/main/Squeak'
 
-export { Squeak, Question }
+export { Squeak, Question, Form }
