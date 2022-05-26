6c76fb91e9757a5db9c62c84ebf25ed4d45abb12
Eli Kinsey <eli@ekinsey.dev>
2022-05-26 11:13:13 -0700

    Add topic buttons (#45)
    * add topics
    
    * add all topics button

--- src/components/Questions.js
+++ src/components/Questions.js
@@ -3,19 +3,53 @@ import { useOrg } from '../hooks/useOrg'
 import Question from './Question'
 import QuestionForm from './QuestionForm'
 
+const Topics = ({ handleTopicChange, activeTopic }) => {
+  const { topics } = useOrg()
+
+  return (
+    topics &&
+    topics.length > 0 && (
+      <ul className='squeak-topics-container'>
+        <li>
+          <button
+            className={activeTopic === null ? 'squeak-active-topic' : ''}
+            onClick={() => handleTopicChange(null)}
+          >
+            All
+          </button>
+        </li>
+        {topics.map(({ label }) => {
+          return (
+            <li>
+              <button
+                className={activeTopic === label ? 'squeak-active-topic' : ''}
+                onClick={() => handleTopicChange(label)}
+              >
+                {label}
+              </button>
+            </li>
+          )
+        })}
+      </ul>
+    )
+  )
+}
+
 export default function Questions({
   slug = window.location.pathname.replace(/\/$/, '')
 }) {
+  const [activeTopic, setActiveTopic] = useState(null)
   const { organizationId, apiHost } = useOrg()
   const [questions, setQuestions] = useState([])
-  const getQuestions = async () => {
+  const getQuestions = async (topic) => {
     const response = await fetch(`${apiHost}/api/questions`, {
       method: 'POST',
       body: JSON.stringify({
         organizationId,
         slug,
         published: true,
-        perPage: 100
+        perPage: 100,
+        topic
       })
     })
 
@@ -40,8 +74,17 @@ export default function Questions({
     })
   }
 
+  const handleTopicChange = (topic) => {
+    if (topic === activeTopic) return
+    getQuestions(topic).then((questions) => {
+      setQuestions(questions)
+      setActiveTopic(topic)
+    })
+  }
+
   return (
     <>
+      <Topics handleTopicChange={handleTopicChange} activeTopic={activeTopic} />
       {questions && questions.length > 0 && (
         <>
           <ul className='squeak-questions'>
