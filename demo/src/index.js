a243f870d157b01941d8eca3819fc42c0a9f52f1
Alex Sharp <ajsharp@gmail.com>
2022-08-09 13:31:58 -0700

    remove supabase config from demo component

--- demo/src/index.js
+++ demo/src/index.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+import { Squeak } from '../../src/components/main/Squeak'
+
 ReactDOM.render(
   <div style={{ maxWidth: 450 }}>
     <Squeak
-      apiHost='YOUR_API_HOST'
-      apiKey='YOUR_SUPABASE_ANON_KEY'
-      url='YOUR_SUPABASE_URL'
-      organizationId='YOUR_ORGANIZATION_ID'
+      apiHost='http://localhost:3001'
+      organizationId='a898bcf2-c5b9-4039-82a0-a00220a8c626'
     />
   </div>,
   document.getElementById('demo')
